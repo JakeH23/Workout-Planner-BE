c9535e2ae0c979c1f8ff702d1fb2157769a352ee
Joseph <josephboffey1984@gmail.com>
2019-01-21 17:14:20 +0000

    delete workout added

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,9 +1,12 @@
-const workoutRouter = require('express').Router();
-const { getSingleWorkout, postNewWorkout } = require('../controllers/workouts');
+const workoutRouter = require("express").Router();
+const {
+  getSingleWorkout,
+  postNewWorkout,
+  deleteWorkout
+} = require("../controllers/workouts");
 
-
-workoutRouter.route('/').post(postNewWorkout);
-workoutRouter.route('/:workout_name').get(getSingleWorkout);
-
+workoutRouter.route("/").post(postNewWorkout);
+workoutRouter.route("/:workout_name").get(getSingleWorkout);
+workoutRouter.route("/:workout_id").delete(deleteWorkout);
 
 module.exports = workoutRouter;
