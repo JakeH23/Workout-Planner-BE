3f059cb1eae21d3e761faf945ba83f0c6510513c
Joseph <josephboffey1984@gmail.com>
2019-01-22 16:51:50 +0000

    added test data

--- db/seed.dev.js
+++ db/seed.dev.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
-const { seedDB } = require('../db/seed');
-const { DB_URL } = require('../config');
-const muscles = require('./testData/muscles.json');
+const mongoose = require("mongoose");
+const { seedDB } = require("../db/seed");
+const { DB_URL } = require("../config");
+const muscles = require("./testData/muscles.json");
 
-
-mongoose.connect(DB_URL).then(() => seedDB({ muscles }))
+mongoose
+  .connect(
+    DB_URL,
+    { useNewUrlParser: true }
+  )
+  .then(() => seedDB({ muscles }))
   .then(() => mongoose.disconnect())
-  .catch((err) => console.log(err));
-
-
+  .catch(err => console.log(err));
