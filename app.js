8282e4a4767938cac2827e62d80dfb8b4dfc1233
Joseph <josephboffey1984@gmail.com>
2019-01-22 15:28:59 +0000

    config added

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const app = require('express')();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const apiRouter = require('./routes/api');
+const app = require("express")();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const apiRouter = require("./routes/api");
+const mongoose = require("mongoose");
+const { DB_URL } =
+  process.env.NODE_ENV === "production" ? process.env : require("./config");
 
+mongoose
+  .connect(DB_URL)
+  .then(console.log("connected"))
+  .catch(console.log);
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api', apiRouter);
-
+app.use("/api", apiRouter);
 
 module.exports = app;
