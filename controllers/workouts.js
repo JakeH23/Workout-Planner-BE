74820fba83a5af465bf7822809c34e459c9fc302
charlie-wild <charliewild@gmail.com>
2019-01-23 10:02:45 +0000

    adding delete workout controller

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -11,15 +11,19 @@ exports.getAllWorkouts = (req, res, next) => {
 };
 
 exports.getSingleWorkout = (req, res, next) => {
-  Workouts.find({ name: req.params.name })
-  .then((workout) => {
-    if (!workout) return Promise.reject({ status: 404, msg: 'workout not found'});
-    [ workout ] = workout;
-    res.status(200).send({ workout });
-  })
-  .catch(next);
+  Workouts.find({ name: req.params.workout_name })
+    .then((workout) => {
+      if (!workout) return Promise.reject({ status: 404, msg: 'workout not found'});
+      [ workout ] = workout;
+      res.status(200).send({ workout });
+    })
+    .catch(next);
 };
 
 exports.deleteWorkout = (req, res, next) => {
-
+  Workouts.remove({ name: req.params.workout_name })
+    .then(() => {
+      return res.status(204).send({ msg: 'Successful deletion'});
+    })
+    .catch(next);
 };
