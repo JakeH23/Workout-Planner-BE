df0efd7b4be41ddb6b20c127fab92375af30b0bc
Joseph <josephboffey1984@gmail.com>
2019-01-23 10:28:52 +0000

    updates to seed

--- utils/index.js
+++ utils/index.js
@@ -10,3 +10,19 @@ exports.formatExercises = (exercises, userDocs) => {
     };
   });
 };
+
+exports.formatWorkouts = (workouts, exerciseDocs, userDocs) => {
+  return workouts.map(workout => {
+    const findExerciseId = exerciseDocs.find(exercise => {
+      return workout.exercises.map(exercise => exercise === exercise.name);
+    })._id;
+    const findUserId = userDocs.find(user => {
+      return user.user_name === workout.created_by;
+    })._id;
+    return {
+      ...workout,
+      created_by: findUserId,
+      exercises: findExerciseId
+    };
+  });
+};
