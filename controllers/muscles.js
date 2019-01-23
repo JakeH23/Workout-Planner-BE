619a0f33b85c21c23ebe5717b6ef124803f0f8f4
Joseph <josephboffey1984@gmail.com>
2019-01-23 14:53:31 +0000

    updates

--- controllers/muscles.js
+++ controllers/muscles.js
@@ -1,4 +1,4 @@
-const Muscles = require('../models/Muscles');
+const Muscles = require("../models/Muscles");
 
 exports.getSingleMuscle = (req, res, next) => {
   Muscles.find({ muscle_name: req.params.muscle_name })
