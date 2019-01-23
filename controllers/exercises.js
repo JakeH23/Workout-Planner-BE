36766ed69cbd0125851d88252beb1f3a8707697b
Joseph <josephboffey1984@gmail.com>
2019-01-23 11:39:47 +0000

    get exercises

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,14 +1,25 @@
-
+const Exercise = require("../models/Exercise");
 
 exports.getAllExercises = (req, res, next) => {
-
+  Exercise.find()
+    .then(exercises => {
+      if (!exercises.length)
+        return Promise.reject({ status: 404, msg: "exercise not found" });
+      res.send({ exercises });
+    })
+    .catch(next);
 };
 
-
-exports.getSingleExercise = (req, res, next) => {
-
-};
+exports.getSingleExercise = (req, res, next) => {};
 
 exports.postNewExercise = (req, res, next) => {
-
+  Exercise.create(req.body)
+    .then(exercise => {
+      return res.status(201).send({ exercise });
+    })
+    .catch(err => {
+      if (err.name === "ValidationError") {
+        next({ status: 400, msg: "bad post request" });
+      }
+    });
 };
