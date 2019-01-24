addd397c930619fe8170156e9f60bc60c1e6c3fd
Joseph <josephboffey1984@gmail.com>
2019-01-24 15:28:57 +0000

    add completed workout edits

--- controllers/completedworkouts.js
+++ controllers/completedworkouts.js
@@ -3,25 +3,36 @@ const Workouts = require('../models/Workout');
 
 exports.getUserCompletedWorkouts = (req, res, next) => {
   CompletedWorkouts.find()
-  .then((completedWorkouts) => {
+    .then((completedWorkouts) => {
       const userCompleted = completedWorkouts.map(workout => {
-   if (workout.user_name === req.params.username) {
-   return workout;
+        if (workout.user_name === req.params.username) {
+          return workout;
         };
       });
-    res.status(200).send({ userCompleted });
-  })
+      res.status(200).send({ userCompleted });
+    })
     .catch(next);
 };
 
-exports.addCompletedWorkout = (req, res, next) => {
-  const completedWorkout = findCompletedWorkout(req.params.name)
-  CompletedWorkouts.create(completedWorkout)
+exports.addCompletedWorkout = async (req, res, next) => {
+  const completedWorkout = await findCompletedWorkout(req.params.workout_name)
+  console.log(completedWorkout)
+  const { _id, created_by } = completedWorkout[0]
+  const newCompletedWorkout = {
+    workout: _id,
+    user_id: created_by
+  }
+  console.log(newCompletedWorkout)
+  CompletedWorkouts.create(newCompletedWorkout)
     .then(workout => res.status(201).send({ workout }))
     .catch((next));
 };
 
 
-const findCompletedWorkout = (workoutName) => {
-  return Workouts.find({ name: workoutName })
+const findCompletedWorkout = async (workoutName) => {
+  const newWorkout = await Workouts.find({ name: workoutName }).then(workout => (workout))
+
+  return newWorkout;
+
+
 }
