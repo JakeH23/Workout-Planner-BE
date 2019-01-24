68f669bda7075c13a74fe7e038a72ca8ed1c9e6f
Joseph <josephboffey1984@gmail.com>
2019-01-24 09:54:06 +0000

    update delete

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
-const ObjectId = require('mongodb');
-const Workouts = require('../models/Workout');
-const Users = require('../models/Users');
-const Exercises = require('../models/Exercise');
+const mongoose = require("mongoose");
+const ObjectId = require("mongodb");
+const Workouts = require("../models/Workout");
+const Users = require("../models/Users");
+const Exercises = require("../models/Exercise");
 
 exports.getAllWorkouts = (req, res, next) => {
   Workouts.find()
-    .then((workouts) => {
-      if (!workouts.length) return Promise.reject({ status: 404, msg: 'workouts not found' });
+    .then(workouts => {
+      if (!workouts.length)
+        return Promise.reject({ status: 404, msg: "workouts not found" });
       res.status(200).send({ workouts });
     })
     .catch(next);
@@ -15,8 +16,9 @@ exports.getAllWorkouts = (req, res, next) => {
 
 exports.getSingleWorkout = (req, res, next) => {
   Workouts.find({ name: req.params.workout_name })
-    .then((workout) => {
-      if (!workout) return Promise.reject({ status: 404, msg: 'workout not found' });
+    .then(workout => {
+      if (!workout)
+        return Promise.reject({ status: 404, msg: "workout not found" });
       [workout] = workout;
       res.status(200).send({ workout });
     })
@@ -28,9 +30,9 @@ exports.postNewWorkout = (req, res, next) => {
     name: req.body.name,
     exercises: req.body.exercises,
     private: req.body.private,
-    created_by: req.body.created_by,
+    created_by: req.body.created_by
   })
-    .then((newWorkout) => {
+    .then(newWorkout => {
       res.status(201).send({ newWorkout });
     })
     .catch(next);
@@ -38,6 +40,9 @@ exports.postNewWorkout = (req, res, next) => {
 
 exports.deleteWorkout = (req, res, next) => {
   Workouts.deleteOne({ name: req.params.workout_name })
-    .then(() => res.status(204).send({ msg: 'Successful deletion' }))
+    .then(
+      () =>
+        console.log(res) || res.status(204).send({ msg: "Successful deletion" })
+    )
     .catch(next);
 };
